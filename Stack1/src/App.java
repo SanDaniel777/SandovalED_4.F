34cbd25cbb3c7b91538db008d83df930aab264e9
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-11 19:55:59 -0600

    Se agregaron las 3 paginas y se utilizo el priimer pop

--- Stack1/src/App.java
+++ Stack1/src/App.java
@@ -1,5 +1,21 @@
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola soy: Kevin Daniel Sandoval Rueda!");
+
+//Inicializacion del stack
+        Deque<String> webs = new ArrayDeque<>();
+        webs.push("Pagina web : SISA UTEZ");
+        webs.push("Pagina web : CODEC UTEZ");
+        webs.push("Pagina web : UTEZ");
+        System.out.println("Stack ahorita:" + webs);
+
+        System.out.println("Quitamos la pagina: " +webs.pop());
+
+
+
     }
 }
