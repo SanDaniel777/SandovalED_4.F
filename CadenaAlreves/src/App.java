772ae35f33736b637b797cd26546930f31228445
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-19 08:18:40 -0600

    se termino el ejercicio

--- CadenaAlreves/src/App.java
+++ CadenaAlreves/src/App.java
@@ -3,5 +3,11 @@ public class App {
         System.out.println("Kevin Daniel");
         String og = "Hola";
         String alreves = "";
+
+        for(int i = og.length()-1; i>=0 ;i--){
+            alreves += og.substring(i,i+1);
+        }
+        System.out.println(alreves);
+
     }
 }
