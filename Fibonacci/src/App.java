965e9b0b5b451e26918cecd519838bf710416f8b
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-18 19:28:08 -0600

    se creo el codigo fibonacci

--- Fibonacci/src/App.java
+++ Fibonacci/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Kevin Daniel Sandoval!");
+           int n=5;
+             
+            System.out.println(" los primeros numeros de la serie fibonacci " +n+ " son:");
+            for(int i = 0; i < n; i++){
+                System.out.println("Fibo:" +i+ + fibonacci(i));
+            }
+        
+        
+
+
+
     }
-}
+    
+    static int fibonacci(int n){
+            if (n <= 1) return n;
+            return fibonacci(n -1)+fibonacci(n-2);
+        }
+
+    }
