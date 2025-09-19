4f5ec953ed6122903578611ebc92029ac0abd037
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-19 08:53:42 -0600

    se corrigio

--- CadenaAlreves/src/App.java
+++ CadenaAlreves/src/App.java
@@ -2,12 +2,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kevin Daniel");
         String og = "Hola";
-        String alreves = "";
-
-        for(int i = og.length()-1; i>=0 ;i--){
-            alreves += og.substring(i,i+1);
-        }
-        System.out.println(alreves);
-
+        System.out.println(og.charAt(0));
+        reverseWord(og.length()-1,og);
     }
+
+    public static void reverseWord(int index, String og){
+        if (index == 0) {
+            System.out.print(og.charAt(0));
+            return;
+        }
+        System.out.print(og.charAt(index)+"");
+        reverseWord(index-1, og);
+    }
+
 }
