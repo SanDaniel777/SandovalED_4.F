3062a28f32453d22f2a7f58ba53855b189c3b6ed
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-20 20:48:08 -0600

    se arreglo el bug

--- PotenciaRecu/src/App.java
+++ PotenciaRecu/src/App.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kevin Daniel");
-
-
-
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Agrega el primer numero: ");
+        int n1 = sc.nextInt();
+        System.out.println("Agruega su potencia");
+        int p= sc.nextInt();
+        int resultado= potencia(n1,p);
+        System.out.println("el resultado es igual a:" +resultado);
+        sc.close();
     }
+
+    static int potencia(int n1, int p){
+         if (p == 0) {
+            return 1;
+        }
+         return n1 * potencia(n1, p - 1);
+    }
+
 }
