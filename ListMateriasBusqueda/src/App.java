11aeab50a6f48ebbe2e448fe20d2769e22cec4b5
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-22 20:43:38 -0600

    se agregaron las materias a la lista

--- ListMateriasBusqueda/src/App.java
+++ ListMateriasBusqueda/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Materias Sandoval Kevin");
+        Scanner sc = new Scanner(System.in);
+
+        ListaMaterias lista = new ListaMaterias();
+        lista.agregar("Fisica");
+        lista.agregar("Calculo");
+        lista.agregar("Apps Moviles");
+        lista.agregar("Etica");
+        lista.agregar("Programacion");
+
+        lista.imprimir();
+
+
+
+
     }
 }
