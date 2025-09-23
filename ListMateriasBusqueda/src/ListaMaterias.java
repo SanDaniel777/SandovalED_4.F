d5a2cb5bdea117dd6a9649a51eb3699e9e3ce0a5
SanDaniel777 <20243ds156@utez.edu.mx>
2025-09-22 20:32:35 -0600

    Se terminaron todas las funciones de la clase materias

--- ListMateriasBusqueda/src/ListaMaterias.java
+++ ListMateriasBusqueda/src/ListaMaterias.java
@@ -1,10 +1,29 @@
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 
 public class ListaMaterias {
+private List<String> materias;
 
+public void ListaMaterias(){
+    materias = new ArrayList<>();
+}
 
-    
+public void agregar(String materia){
+    materias.add(materia);
+}
+
+public boolean contiene(String materia){
+    for(String m : materias){
+        if (m.equalsIgnoreCase(materia)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+public void imprimir(){
+    System.out.println("Materias: " +materias);
+}
+
     
 }
