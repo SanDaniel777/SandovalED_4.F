94175a9e38e60d62d53b1161700319f41e90f5ba
SanDaniel777 <20243ds156@utez.edu.mx>
2025-11-07 07:58:24 -0600

    se entrego el bubblesort

---
+++ BurbleSort/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] arr={1,5,9,10,3,-4};
+
+        bubbleSort.sort(arr);
+
+    }
+}
