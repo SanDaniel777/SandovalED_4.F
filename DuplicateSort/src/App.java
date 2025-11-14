97bf63bef71f33f9a16a1d41bc4c098ff26b4a52
SanDaniel777 <20243ds156@utez.edu.mx>
2025-11-14 08:56:01 -0600

    se termino la tarea de quitar duplicados y bubbleSort

--- DuplicateSort/src/App.java
+++ DuplicateSort/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        BubleSort bubleSort=new BubleSort();
+          int[] arr = {4, 2, 9, 2, 4, 7, 1};
+
+        System.out.println("Arreglo original:");
+        System.out.println(Arrays.toString(arr));
+
+    
+        bubleSort.bubbleSort(arr);
+
+        System.out.println("Arreglo después de ordenar:");
+        System.out.println(Arrays.toString(arr));
+
+        
+        int[] sinDuplicados = bubleSort.quitarDuplicados(arr);
+
+        System.out.println("Arreglo sin duplicados:");
+        System.out.println(Arrays.toString(sinDuplicados));
     }
 }
--- DuplicateSort/src/BubleSort.java
+++ DuplicateSort/src/BubleSort.java
@@ -1,3 +1,44 @@
 public class BubleSort {
+    
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
 
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+     public static int[] quitarDuplicados(int[] arr) {
+
+
+        int contador = 1;  
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                contador++;
+            }
+        }
+
+
+        int[] resultado = new int[contador];
+
+    
+        resultado[0] = arr[0];
+        int index = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                resultado[index] = arr[i];
+                index++;
+            }
+        }
+
+        return resultado;
+    }
 }
+
