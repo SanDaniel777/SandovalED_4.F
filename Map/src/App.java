4039ee0c06373386ba1c00a4f0a7c63059f750b7
SanDaniel777 <20243ds156@utez.edu.mx>
2025-11-28 08:08:01 -0600

    se termino MAP

--- Map/src/App.java
+++ Map/src/App.java
@@ -11,8 +11,22 @@ public class App {
         map.put(1, "Mouse");
         map.put(2, "Teclado");
         map.put(3, "Monitor");
-        map.put(1, "Mouse Gamer");
+        map.put(1, "Mouse Gamer");//se sustituye por la key 1
 
         System.out.println(map);
+
+        String data=  map.get(1);//obtener valor por key
+        System.out.println(data);
+        boolean exist= map.containsValue("teclado");//verificar si existe x objeto
+        System.out.println("Hay un teclado: "+ exist);
+        map.remove(1);
+
+        for(Map.Entry<Integer, String>row:map.entrySet()){
+            System.out.println("La key es: "+row.getKey()+" el valor es: "+row.getValue());
+        }
+
+        boolean existKey=map.containsKey(1);//verificar si existe x key
+        System.out.println("existe la key 1? "+ existKey);
+
     }
 }
